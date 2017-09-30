46505c36681480d1f0b88d2c6182923d263bbe9b
Emil Rowland <rowland.emil@gmail.com>
2017-09-30 23:06:16 +0200

    Added more unittest

--- tests/test_movingBox.py
+++ tests/test_movingBox.py
@@ -15,3 +15,9 @@ class TestMovingBox(unittest.TestCase):
 
     def test_create_database(self):
         self.assertTrue(Path(self.path_to_db))
+
+    def test_add_moving_box(self):
+        box_id = self.db.add_moving_box('content1', 'place1')
+        self.assertTrue(self.db.get_moving_box(box_id))
+        self.db.add_moving_box('content2', 'place2')
+        self.assertEqual(len(self.db.get_all_moving_boxes()), 2)
