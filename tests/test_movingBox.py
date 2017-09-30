5efc7119e8bdc4937d6a98f3817199fab3836bf8
Emil Rowland <rowland.emil@gmail.com>
2017-09-30 23:11:41 +0200

    Fixed test

--- tests/test_movingBox.py
+++ tests/test_movingBox.py
@@ -17,7 +17,14 @@ class TestMovingBox(unittest.TestCase):
         self.assertTrue(Path(self.path_to_db))
 
     def test_add_moving_box(self):
-        box_id = self.db.add_moving_box('content1', 'place1')
-        self.assertTrue(self.db.get_moving_box(box_id))
-        self.db.add_moving_box('content2', 'place2')
-        self.assertEqual(len(self.db.get_all_moving_boxes()), 2)
+        box_id_1 = self.db.add_moving_box('content1', 'place1')
+        self.assertTrue(self.db.get_moving_box(box_id_1))
+        box_id_2 = self.db.add_moving_box('content2', 'place2')
+        self.assertTrue(self.db.get_moving_box(box_id_1))
+        self.assertTrue(self.db.get_moving_box(box_id_2))
+        self.assertEqual(len(self.db.get_all_moving_boxes().fetchall()), 2)
+
+    def test_get_moving_box(self):
+        box_id_1 = self.db.add_moving_box('content1', 'place1')
+        self.assertTrue(self.db.get_moving_box(box_id_1))
+        self.assertFalse(self.db.get_moving_box(box_id_1+10))
