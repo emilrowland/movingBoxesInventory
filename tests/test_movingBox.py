18bd826b97a90c61a0cecf99d5df58efce1ea9a1
Emil Rowland <rowland.emil@gmail.com>
2017-09-30 22:17:33 +0200

    Testing

---
+++ tests/test_movingBox.py
@@ -0,0 +1,17 @@
+import unittest
+import os
+from pathlib import Path
+from movingBox import MovingBox
+
+
+class TestMovingBox(unittest.TestCase):
+    def setUp(self):
+        self.db = MovingBox()
+        self.path_to_db = 'movingBoxes.db'
+
+    def tearDown(self):
+        del self.db
+        os.remove(self.path_to_db)
+
+    def test_create_database(self):
+        self.assertTrue(Path(self.path_to_db))
