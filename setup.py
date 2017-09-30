647134f6f815ed9d38b81c9757cdbe2b6959bc3d
Emil Rowland <rowland.emil@gmail.com>
2017-09-30 22:50:27 +0200

    Small fix

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
 from setuptools import setup
 
-setup(name='funniest',
+setup(name='movingBoxesInventory',
       version='0.1',
       description='An small python app to keep track of your moving boxes',
       url='https://github.com/emilrowland/movingBoxesInventory',
       license='MIT',
       packages=['movingBoxesInventory'],
+      test_suite="tests",
       zip_safe=False)
